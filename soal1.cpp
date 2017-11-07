475688c4a50cce34ede8c510a30a28d6327bf1e4
Muhammad Fikry Nisfuadi <32459391+mfikrinisfuadi@users.noreply.github.com>
2017-11-08 01:56:55 +0700

    membuat hasil selalu positif

--- soal1.cpp
+++ soal1.cpp
@@ -7,4 +7,9 @@ int main(){
 	int selisih;
 	cout <<"masukan nilai x:"; cin >>x;
 	cout <<"masukan nilai y:"; cin >>y;
+	selisih=x-y;
+	if(selisih<0)
+	cout << selisih*-1;
+	else
+	cout << selisih;
 }
