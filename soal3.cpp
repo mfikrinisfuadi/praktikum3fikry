5b7ee6dd76826caae68b891adf493e6804a33e79
Muhammad Fikry Nisfuadi <32459391+mfikrinisfuadi@users.noreply.github.com>
2017-11-08 03:12:23 +0700

    perhitungan dan output nya

--- soal3.cpp
+++ soal3.cpp
@@ -12,4 +12,14 @@ int main(){
 	int gaber;
 	cout<<"masukan jam kerja anda:"; cin >> jamker;
 	cout<<"masukan gajih anda perjam:"; cin >> gajihph;
+	lembur=jamker-40;
+	jamlembur=lembur*1.5;
+	jamkertotal=40+jamlembur;
+	gatot=jamkertotal*gajihph;
+	gaber=gatot-(0.15*gatot);
+	cout <<"jam lembur anda:"<<lembur<<endl;
+	cout <<"bonus dari lembur anda:"<<jamlembur<<endl;
+	cout <<"jam kerja total anda:"<<jamkertotal<<endl;
+	cout <<"gajih total anda:"<<gatot<<endl;
+	cout <<"gajih bersih anda di potong pajak:"<<gaber<<endl;
 }
