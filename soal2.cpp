1c476559095fd264a341c28f4057e7084520d628
Muhammad Fikry Nisfuadi <32459391+mfikrinisfuadi@users.noreply.github.com>
2017-11-08 02:17:19 +0700

    INPUT DAN PROSES PERHITUNGAN

--- soal2.cpp
+++ soal2.cpp
@@ -6,4 +6,23 @@ int main(){
 	int l;
 	int a;
 	char abjad;
+	cout << "bangun datar apa yang ingin anda hitung? inputkan 'p' untuk persegi dan 's' untuk segitiga:"; cin >> abjad;
+	if (abjad=='p' || abjad=='P' )
+	{
+		cout <<"Anda akan menghitung luas persegi \n"
+		 <<"masukan tinggi:"; cin >> t;
+		cout <<"masukan lebar:"; cin >> l;
+		cout <<"luas persegi anda:";
+		cout <<t*l;
+		}
+		else	if (abjad=='s' || abjad=='S' )
+	{
+		cout <<"Anda akan menghitung luas segitiga \n"
+		 <<"masukan tinggi:"; cin >> t;
+		cout <<"masukan alas:"; cin >> a;
+		cout <<"luas segitiga anda:";
+		cout <<0.5*a*t;
+		}
+		else
+		cout <<"tidak tersedia";
 }
