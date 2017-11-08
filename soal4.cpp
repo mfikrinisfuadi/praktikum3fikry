8c2b16ce57b625c713f43b840eb701010a959608
Muhammad Fikry Nisfuadi <32459391+mfikrinisfuadi@users.noreply.github.com>
2017-11-08 10:00:05 +0700

    menginput dan mengoutputkan hasil bilangan

--- soal4.cpp
+++ soal4.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main(){
 	int x;
+	
+	cout<<"Input angka : ";cin>>x;
+	if (x < 0)
+	{
+		cout<<"bilangan yang anda input negatif"<<endl;
+	}
+    else if (x>0)
+	{
+		cout<<"bilangan yang anda input positif"<<endl;
+	}
+    else
+	
+		cout<<"bilangan yang anda input nol "<<endl;
+
 }
 
